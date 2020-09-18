3c369c07e9fc0e9e2a379607839017ece237612b
Dominic01prog <djs7129@psu.edu>
2020-09-18 03:03:03 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+// Author: Dominic Savaglio
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int digit_sum(int n){
+  if (n == 0) {
+    return 0;
+  } else {
+    return n % 10 + digit_sum(n / 10);
+  }
+}
+int main(void) {
+  char *sum = readline("Enter an int: ");
+  int add = atof(sum);
+  digit_sum(add);
+  printf("sum of the digits of %s is %d.\n", sum, digit_sum(add));
+}
